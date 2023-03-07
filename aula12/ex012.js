75de83cdf7b0e1fc3369e6e961c61627ce16f3ea
TMMDSS <souzatailam@gmail.com>
2023-03-07 06:02:21 -0300

    fiz o ex012

---
+++ aula12/ex012.js
@@ -0,0 +1,9 @@
+var hora = 7
+console.log(`agora são exatamente ${hora} horas`)
+if (hora <= 12) {
+    console.log('agora é de manhã')
+} else if (hora < 18) {
+    console.log('agora é de tarde')
+} else {
+    console.log('agora é de noite')
+}
