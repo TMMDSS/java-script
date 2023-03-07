6ebd74bf7101204c71f59e1d23f43512f138e9c9
TMMDSS <souzatailam@gmail.com>
2023-03-07 06:23:02 -0300

    adcionei madrugada no ex012

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,9 +1,11 @@
 var hora = 7
 console.log(`agora são exatamente ${hora} horas`)
-if (hora <= 12) {
+if (hora >= 7 && hora <= 12) {
     console.log('agora é de manhã')
-} else if (hora < 18) {
+} else if (hora < 18 && hora > 12) {
     console.log('agora é de tarde')
-} else {
+} else if (hora >= 18) {
     console.log('agora é de noite')
+} else if (hora >= 0 && hora < 7) {
+    console.log('agora é madrugada')
 }
