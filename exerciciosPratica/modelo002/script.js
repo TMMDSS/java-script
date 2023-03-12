2fad6f647d34992d0db7fa379dab9981cbab2a35
TMMDSS <souzatailam@gmail.com>
2023-03-12 09:17:19 -0300

    update

--- exerciciosPratica/modelo002/script.js
+++ exerciciosPratica/modelo002/script.js
@@ -15,12 +15,39 @@ function verificar() {
             genero = 'homem'
             if (idade >= 0 && idade < 10) {
                 //criança
-            } else if (idade >= 10 && idade < 21 ) {
+                img.setAttribute('src', 'criança-mas.jpg')
+            } else if (idade <= 21 ) {
                 //jovem
-            } else if (idade > 50) {
+                img.setAttribute('src', 'jovem-mas.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-mas.jpg')
+
+            } else {
                 //idoso
+                img.setAttribute('src', 'idoso-mas.jpg')
             }
-        } else if (fsex[1].checked) {
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        else if (fsex[1].checked) {
             genero = 'mulher'
             if (fsex[1].checked) {
                 genero = 'mulher'
@@ -35,5 +62,6 @@ function verificar() {
         }      
         res.style.textAlign = 'center'
         res.innerHTML = `detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
