7cf4f38bcfc9d04f458c683693aeb3c28bbc3ef4
TMMDSS <souzatailam@gmail.com>
2023-03-12 07:00:47 -0300

    consertei o codigo do ex013

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -16,7 +16,8 @@ console.log(diaSem)
 switch(diaSem) {
     case 0:
         console.log('Domingo')
-
+        break
+        
         case 1:
             console.log('Segunda')
             break
